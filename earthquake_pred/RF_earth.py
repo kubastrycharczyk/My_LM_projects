1d3f439bd2158ecac4791fd897444ddbbeb81060
Jakub Strycharczyk <kuba.ja.strycharczyk@gmail.com>
2025-09-03 11:00:28 +0200

    backbone of class is ready

--- earthquake_pred/RF_earth.py
+++ earthquake_pred/RF_earth.py
@@ -6,7 +6,11 @@ target_name = "Magnitude"
 
 
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+
 from sklearn import preprocessing
+
+from sklearn.ensemble import RandomForestClassifier
 
 
 y = dataframe[target_name]
@@ -16,23 +20,34 @@ print(x)
 class Forest_Predictor:
     def standarizer(z):
         minmax_scaler = preprocessing.MinMaxScaler(feature_range=(0,1))
-        z=minmax_scaler.fit_transform(z)
+        return minmax_scaler.fit_transform(z)
   
     def __init__(self, dataframe, target_name):
         y = dataframe[target_name].to_numpy()
         X = dataframe.drop(target_name, axis=1).to_numpy()
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X,y, train_size = 0.8)
-        self.standarizer(self.X_test)
-        self.standarizer(self.X_train)
-        self.standarizer(self.y_test)
-        self.standarizer(self.y_train)
+        self.X_test = self.standarizer(self.X_test)
+        self.X_train = self.standarizer(self.X_train)
 
-    def setting_parameters():
-        pass
+        self.n_jobs=-1 #how many processors in usage
+        self.random_state = 0 
+        self.n_esimators = 100
 
-    def train_forest():
-        pass
+
+    def setting_parameters(self,n_jobs, random_state, n_estimators):
+        self.n_jobs=n_jobs
+        self.random_state = random_state
+        self.n_esimators = n_estimators
+
+    def train_forest(self):
+        self.model = RandomForestClassifier(random_state=self.random_state, n_jobs=self.n_jobs, n_estimators=self.n_esimators).fit(self.X_train, self.y_train)
+        score = cross_val_score(self.model, self.X, self.y, cv=5, scoring="accuracy")
+
+    def test_forest(self):
+        prediction=self.model.predict(self.X_test)
+        predictio=prediction-self.y_test
+        return prediction
     
-    def predict_forest():
-        pass
+    def predict_forest(self, exa):
+        self.model.predict(exa) 
 
