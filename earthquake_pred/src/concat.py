4b66da5d804e40c965fda0cea854c15828a6421c
Jakub Strycharczyk <kuba.ja.strycharczyk@gmail.com>
2025-09-24 23:53:33 +0200

    structuring project into folders and adding new distance metric of haversine to cuncat

--- earthquake_pred/src/concat.py
+++ earthquake_pred/src/concat.py
@@ -6,18 +6,34 @@ from sklearn.preprocessing import LabelEncoder
 
 
 class concat:
-    def __init__(self, file_path_main, file_path_add):
+    def __init__(self, file_path_main, file_path_add, dist_met='haversine'):
         self.df = pd.read_csv(file_path_main)
         self.df_add = pd.read_csv(file_path_add)
+        self.dist_methods = {
+            "euclidean": self._eucl_dist,
+            "haversine": self._have_dist
+        }
+        self.dist_metric = dist_met
+    
+    def _rad(self, z):
+        return math.pi * z / 180 
+
+    def _have_dist(self, x,y, a,b):
+        x, y, a, b = self._rad(x),self._rad(y), self._rad(a), self._rad(b)
+        R = 6371
+        v=math.sin((x-a)/2)**2 + math.cos(x) * math.cos(a) * math.sin((y-b)/2)**2
+        return 2*R*math.atan2(math.sqrt(v), math.sqrt(1 - v))
 
     def _eucl_dist(self,x,y, a,b):
         return math.sqrt((x-a)**2 + (y-b)**2)
         
     def _dist_calc(self, x, y):
+        if self.dist_metric not in self.dist_methods:
+            raise ValueError("Inproper metric, choose euclidean or haversine")
         min_dist = float('inf')
         plate = ""
         for i, j, z in zip(self.df_add["lat"], self.df_add["lon"], self.df_add["plate"]):
-            tmp = self._eucl_dist(x, y, i, j)
+            tmp = self.dist_methods[self.dist_metric](x, y, i, j)
             if min_dist > tmp:
                 min_dist = tmp
                 plate = z
